cb1125f27b5587dd2d70bcc2bc5a3e0501e81e04
Johanna Raisa <johanna.raisa@gmail.com>
2018-08-10 10:35:02 +0300

    Dumps params

--- app.pl
+++ app.pl
@@ -3,13 +3,11 @@ use Mojolicious::Lite;
 use JSON;
 use Data::Dumper;
 
-
-
 app->config(hypnotoad => {listen => ['http://*:8081']});
 
 post '/event_handler' => sub {
   my $c = shift;
-  print Dumper $c->req;
+  print Dumper from_json($c->param{"payload"});
   $c->render(json => $c->req->body);
 };
 
