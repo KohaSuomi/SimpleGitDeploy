0c69b3dd398ce153c22f82477ac9004903efeebd
janPasi <pasi.korkalo@ouka.fi>
2018-11-01 13:44:49 +0200

    Fix working directory when running as initscript

--- scripts/simple_git_deploy_daemon.sh
+++ scripts/simple_git_deploy_daemon.sh
@@ -9,11 +9,11 @@
 # Short-Description: Hypnotoad Mojolicious Server for handling API requests
 ### END INIT INFO
 
-BASEDIR=$(dirname "$0")
+BASEDIR="$(dirname "$(readlink --canonicalize "$0")")"
 
 if [[ $EUID -ne 0 ]]; then
-    echo "You must run this script as 'root'";
-    exit 1;
+    echo "You must run this script as 'root'"
+    exit 1
 fi
 
 function start {
